d1256bb15e16cc85e64d8e1d2ce7981f1e268393
alfred luo <yunshu_luo@163.com>
2020-04-04 20:58:20 +0800

    <feat> 足迹列表代码完善

--- app/Business/FootprintBusiness.php
+++ app/Business/FootprintBusiness.php
@@ -3,10 +3,10 @@
 namespace App\Business;
 
 use App\Helper\CommonResult;
-use App\Models\Mysql\Footprint;
 
 class FootprintBusiness extends BaseBusiness
 {
+    protected static $model = 'Footprint';
     protected static $select = ['id', 'goods_id', 'created_at'];
 
     public static function getList(array $attributes)
@@ -40,31 +40,5 @@ class FootprintBusiness extends BaseBusiness
         }
 
         return CommonResult::formatBody(array_merge($page, ['list'=>$footprintList]));
-    }
-
-    protected static function queryListByCondition($page, $limit, $condition=[], $sort='created_at', $order='desc', $select='', $with=[])
-    {
-        $query = Footprint::query();
-        $select = $select ? $select : self::$select;
-        if ($with) {
-            $query = $query->with($with);
-        }
-
-        return $query->where($condition)
-                    ->orderBy($sort, $order)
-                    ->forPage($page, $limit)
-                    ->select($select)
-                    ->get()
-                    ->toArray();
-    }
-
-    protected static function queryCountByCondition($condition)
-    {
-        $query = Footprint::query();
-        if ($condition) {
-            $query = $query->where($condition);
-        }
-
-        return $query->count();
     }
 }
